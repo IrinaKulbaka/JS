e618e4547d86dbf4ab12ce59ffd34d7fb3d85c90
Irina <constance100478@gmail.com>
2024-09-26 20:31:14 +0300

    init commit

---
+++ Homework/hw_8/main.js
@@ -0,0 +1,42 @@
+// Створити функцію, яка робить глибоку копію об'єкту.
+// Додати перевірки на undefined, null, NaN.
+// Подумати і реалізувати логіку, в якій кінцевий об'єкт буде мати функції,
+// які в нього були до цього моменту.
+
+function cloner(obj){
+    if (obj) {
+        let functions = [];
+        for (const key in obj) {
+            if (typeof obj[key] === 'function'){
+                const functionClone = obj[key].bind({});
+                functions.push({functionClone, key});
+            }
+        }
+        const cloneObj = JSON.parse(JSON.stringify(obj));
+        // cloneObj [functions[0].key] = functions[0].functionClone;
+        // cloneObj[functions[1].key] = functions[1].functionClone;
+        for (const func of functions) {
+            cloneObj [func.key] = func.functionClone;
+        }
+        console.log(cloneObj);
+        return cloneObj;
+    }
+    throw new Error('!!!');
+}
+cloner({id: 12, name:'kokos', greeting(){}, foo(){}});
+
+
+// - є масив
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+// за допомоги map перетворити кожен елемент на наступний тип {id,title,monthDuration
+// Зробити все ВИКЛЮЧНО за допомоги інлайн конструкції
+
+console.log(coursesAndDurationArray.map((course, index) => ({...course, id: index + 1})));
+
